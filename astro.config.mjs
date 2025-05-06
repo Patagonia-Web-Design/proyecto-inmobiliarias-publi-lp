a5f4e6ce27cfcbc696c0b6b84c3e4941ad354446
Alex Srebernic <84547550+alexsrebernic@users.noreply.github.com>
2025-05-05 22:38:45 -0300

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://packdefinitivo.com',
   base: '/',
+  adapter: cloudflare(),
   integrations: [],
   build: {
     assets: 'assets'
